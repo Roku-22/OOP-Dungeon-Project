f3684254b09a705d2d6e64cc998516f005abcaad
Roku-22 <lukedainiel46@gmail.com>
2025-09-20 19:47:14 +0800

    Created class character

--- OOP Dungeon/src/App.java
+++ OOP Dungeon/src/App.java
@@ -1,4 +1,22 @@
 public class App {
+
+    abstract class Character {
+        private String name;
+        private String type;
+        private int hp;
+        private int damage;
+        private int mana;
+
+        abstract void setName(name);
+    }
+
+    class Player extends Character {
+        String playerName;
+        void setName(){
+            
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
